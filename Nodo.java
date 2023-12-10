91c5197e9119a3a302891471aa90d637fb263f6f
Victor89Dzul <71950997+Victor89Dzul@users.noreply.github.com>
2023-12-10 10:05:19 -0600

    Add files via upload

---
+++ Nodo.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class Nodo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Nodo{
+	public int dato;
+	public Nodo izquierdo;
+	public Nodo derecho;
+}
